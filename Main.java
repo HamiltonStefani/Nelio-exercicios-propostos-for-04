533c49066c0f257ff09e7eb6b90ffa85bbf94829
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2023-01-17 15:24:01 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
 
+package exerciciosprorpostosfor01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner hs = new Scanner(System.in);
+		
+		int n = hs.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 !=0) {
+				System.out.println(i);
+			}
+			
+		}
+		
+		hs.close();
+
+	}
